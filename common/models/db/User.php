7ca723a7bccf2df3cf74f075d73dd86a2ed9594a
VienLe <VienLV@peacesoft.net>
2018-06-26 16:10:41 +0700

    set user

---
+++ common/models/db/User.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace common\models\db;
+
+use Yii;
+
+/**
+ * This is the model class for table "user".
+ *
+ * @property int $id
+ * @property string $username
+ * @property string $password_hash
+ * @property string $email
+ * @property string $fullname
+ * @property string $avatar
+ * @property string $addres
+ * @property string $auth_key
+ * @property string $password_reset_token
+ * @property int $status
+ * @property string $created_at
+ * @property string $updated_at
+ */
+class User extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'user';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['status'], 'integer'],
+            [['created_at', 'updated_at'], 'safe'],
+            [['username', 'email', 'fullname', 'auth_key'], 'string', 'max' => 200],
+            [['password_hash', 'avatar', 'addres', 'password_reset_token'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'username' => 'Username',
+            'password_hash' => 'Password Hash',
+            'email' => 'Email',
+            'fullname' => 'Fullname',
+            'avatar' => 'Avatar',
+            'addres' => 'Addres',
+            'auth_key' => 'Auth Key',
+            'password_reset_token' => 'Password Reset Token',
+            'status' => 'Status',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+}
